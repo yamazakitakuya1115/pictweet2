b9b274e71b3ba8315445a951d6b3738ad0799c1c
yamazakitakuya <71809256+yamazakitakuya1115@users.noreply.github.com>
2020-12-07 14:40:49 +0900

    Update tweets_controller.rb

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,16 @@
 class TweetsController < ApplicationController
   def index
+    @tweets = Tweet.all
   end
+
+  def create
+    Tweet.create(tweet_params)
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:content)
+  end
+
 end
