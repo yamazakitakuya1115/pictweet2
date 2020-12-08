d7ebb9f175ec476bd8849c84d450eabb3f71e1e5
yamazakitakuya <71809256+yamazakitakuya1115@users.noreply.github.com>
2020-12-08 18:30:53 +0900

    Update tweets_controller.rb

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -8,6 +8,12 @@ class TweetsController < ApplicationController
     redirect_to root_path
   end
 
+  def destroy
+    tweet = Tweet.find(:id)
+    tweet.delete
+    redirect_to root_path
+  end
+
   private
 
   def tweet_params
